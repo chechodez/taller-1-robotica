73cd03975f2bd330a40d681d7abd22d7e7c721ce
chechodez <79472700+chechodez@users.noreply.github.com>
2021-08-26 14:54:06 -0500

    Update ejemplo.m

--- ejemplo.m
+++ ejemplo.m
@@ -1,3 +1,4 @@
-a=Bycicle(2,3);
-b=a.calcForwardKinematics(4,pi/4);
-[c,d]=a.calcInverseKinematics(b);
+%ejemplo para implementar metodos forward kinematics e inverse kinematics
+a=Bycicle(2,3);%inicialización de la clase con los parámetros wheel base y wheel radius
+b=a.calcForwardKinematics(4,pi/4);%calculo de forward kinematics con velocidad angular 4 y angulo delta pi cuartos
+[c,d]=a.calcInverseKinematics(b);%cálculo de inverse kinematics
